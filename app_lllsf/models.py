792769496aa7e1439b730d4a3393ac27d749905f
Alba <avmanyc@gmail.com>
2023-04-29 16:40:49 -0400

    deletes original AboutUs Model and replaces with new one

--- app_lllsf/models.py
+++ app_lllsf/models.py
@@ -1,19 +1,5 @@
 from django.db import models
 
-# class AboutUs(models.Model):
-#     title = models.CharField(max_length=200)
-#     description = models.TextField()
-#     bullet_point_one = models.CharField(max_length=200)
-#     bullet_point_two = models.CharField(max_length=200)
-#     icon_box_one_title = models.CharField(max_length=200)
-#     icon_box_one_description = models.TextField()
-#     icon_box_two_title = models.CharField(max_length=200)
-#     icon_box_two_description = models.TextField()
-#     image = models.ImageField(upload_to='about_images/')
-
-#     def __str__(self):
-#         return self.title
-
 class AboutUs(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
