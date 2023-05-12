05c0c4c9e9b3d727dd6b2834d17fad35b6638fbc
Alba <avmanyc@gmail.com>
2023-05-12 14:05:25 -0400

    adds tODO

--- app_lllsf/models.py
+++ app_lllsf/models.py
@@ -81,7 +81,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
+#TODO: video section
 class FitnessBlog(models.Model):
     CATEGORIES = (
         ('nutrition', 'Nutrition'),
